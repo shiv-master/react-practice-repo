d2894e33e83fc1c38c3406d36fce82f9a87d3517
shivam <shivammishra04101998@gmail.com>
2022-11-26 21:24:19 +0530

    grid practice

--- src/components/Items.js
+++ src/components/Items.js
@@ -7,6 +7,13 @@ const Items = () => {
             <h5>Dictionary</h5>
             <div>It contains all information.</div>
         </div>
+        <div className='grid_container'>
+            <div className='grid_item grid_item1'>grid item1 Lorem ipsum dolor sit amet.</div>
+            <div className='grid_item grid_item2'>grid item2</div>
+            <div className='grid_item grid_item3'>grid item3</div>
+            <div className='grid_item grid_item4'>grid item4</div>
+            <div className='grid_item grid_item5'>grid item5</div>
+        </div>
         </>
     )
 }
